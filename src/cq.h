0768eab0053812cc7f2871855c4d230cda4e0157
root <zhangqianyu.sys@bytedance.com>
2021-12-27 00:35:56 +0800

    add some class

--- src/cq.h
+++ src/cq.h
@@ -1,6 +1,36 @@
 #ifndef CQ_H
 #define CQ_H
 
+#include <queue>
+
+#include "epoll_handler.h"
+
+
+class CompletionEvent {
+public:
+
+    int id;
+
+};
+
+
+class CompletionQueue : public EpollHandler {
+public:
+
+    int event_fd;
+    std::queue<CompletionEvent> que;
+
+
+    CompletionQueue();
+    virtual ~CompletionQueue();
+
+    virtual int handle(uint32_t event);    
+
+
+    int put_event(CompletionEvent& event);
+
+};
+
 
 
 #endif // CQ_H
--- src/qp.h
+++ src/qp.h
@@ -2,5 +2,38 @@
 #define QP_H
 
 
+#include <queue>
+
+#include "epoll_handler.h"
+
+
+
+class QpRequest {
+public:
+    int id;
+
+
+};
+
+
+class CompletionQeuue;
+
+class QpRequestQueue : public EpollHandler {
+public:
+    int event_fd;
+    std::queue<QpRequest> que;
+
+    CompletionQueue* cq;
+
+    QpRequestQueue();
+    virtual ~QpRequestQueue();
+
+    virtual int handle(uint32_t event);
+
+    int put_request(QpRequest& request);
+
+};
+
+
 
 #endif // QP_H
